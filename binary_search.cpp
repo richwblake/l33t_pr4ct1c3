3e0cbf52fbb0da1a29d2aa912a9a947d9689a225
wills blake <richwblake@gmail.com>
2022-05-12 18:31:32 -0700

    bin search

---
+++ binary_search.cpp
@@ -0,0 +1,33 @@
+#include <iostream>
+#include <vector>
+
+int searchUtil (std::vector<int>& nums, int target, int l, int r) {
+    if (l > r) return -1;
+    int mid = (l + r) / 2;
+    int num = nums[mid];
+    if (num == target) return mid;
+    if (num > target) return searchUtil(nums, target, l, mid - 1);
+    if (num < target) return searchUtil(nums, target, mid + 1, r);
+    return -1;
+}
+int search (std::vector<int>& nums, int target) {
+    int n = nums.size();
+    return searchUtil(nums, target, 0, n);
+}
+
+
+int main () {
+
+    std::vector<int> numbers = { 1, 2, 3, 4, 5, 6, 7, 8, 9 };
+
+    int index = search(numbers, 8);
+
+    std::vector<int> numbers2 = { -8, -6, -5, -3, -2, -1, 3, 4, 6, 7, 9, 12 };
+
+    int index2 = search(numbers2, -5);
+
+    std::cout << "target found at index: " << index << std::endl;
+    std::cout << "target found at index: " << index2 << std::endl;
+    
+    return 0;
+}
